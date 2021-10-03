756859d6cdbaccbb95466f16a509c54b650c9aa3
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-10-03 17:04:38 +0900

    [ADD] Icons

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:toss/theme/theme_config.dart';
 import 'package:toss/view_models/app_provider.dart';
 import 'package:toss/view_models/home_provider.dart';
+import 'package:toss/view_models/sign_up_provider.dart';
 import 'package:toss/views/sign_in/sign_in_screen.dart';
 import 'package:toss/views/sign_up/sign_up_screen.dart';
 
@@ -11,6 +12,7 @@ void main() {
     providers: [
       ChangeNotifierProvider(create: (_) => AppProvider()),
       ChangeNotifierProvider(create: (_) => HomeProvider()),
+      ChangeNotifierProvider(create: (_) => SignUpProvider()),
     ],
     child: MyApp(),
   ));
